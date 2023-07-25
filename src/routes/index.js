cabe649fdfab94d1ea168755e5179475cc80203f
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-25 18:00:31 -0300

    feat: added log router in index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cars from './carRoutes.js'
+import logs from './logRoutes.js'
 
 const routes = (app) => {
   app.route('/').get((req, res) => {
@@ -7,7 +8,7 @@ const routes = (app) => {
       .status(200)
       .send({ titulo: 'API Rest de carros para prova técnica da BHut' })
   })
-  app.use(express.json(), cars)
+  app.use(express.json(), cars, logs)
 }
 
 export default routes
