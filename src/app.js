b85e33299c8dc1e6dab99f8d7ebfc674e03d4072
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-25 18:01:17 -0300

    feat: added morgan, body parser and cors as middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import express from 'express'
+import morgan from 'morgan'
 import db from './configs/connection.js'
 import routes from './routes/index.js'
 
 db.on('error', console.log.bind(console, 'Connection error'))
 db.once('open', () => console.log('Successful connection to the database'))
 const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 routes(app)
 
 export default app
