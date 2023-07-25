ac82533ed1021f1887cb1c060aebf0a65fb8a9dd
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-25 18:33:46 -0300

    fix: using bhut url api from environments

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://api-test.bhut.com.br:3000/api',
+  baseURL: process.env.BHUT_API_BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 })
